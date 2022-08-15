e4e7ad8e1092467f8b159ffd74392b434b506f9d
teresabarker <teresa.barker12@gmail.com>
2022-08-15 14:51:31 -0400

    updated mobile nav hamburger icon to turn into an X while open based on user feedback

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     
     // Toggle Mobile Navigation menu open/closed
-    const menuButton = document.querySelector('.hamburger');
+    const menuButton = document.querySelector('.hamburger-icon');
     const mobileMenu = document.querySelector('.mobile-nav');
 
     menuButton.addEventListener('click', function () {
